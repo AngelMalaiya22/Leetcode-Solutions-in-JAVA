5d3c48153ccff3d60c05dc7bfab501adf2417876
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-20 15:54:17 +0530

    Time: 7 ms (99.97%) | Memory: 47.9 MB (26.11%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -6,13 +6,14 @@ class NumArray {
         this.nums=nums;
         for(int i=1;i<nums.length;i++)
         {
-            nums[i]+=nums[i-1];
+            nums[i] += nums[i-1];
         }
     }
     
     public int sumRange(int left, int right) 
     {
         int sum = nums[right] - (left == 0?0:nums[left-1]);
+
         return sum;
     }
 }
