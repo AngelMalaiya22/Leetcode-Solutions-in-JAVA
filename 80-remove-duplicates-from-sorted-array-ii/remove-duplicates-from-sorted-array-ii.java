ba25067871205ee2468c67074751a38efc2acbb5
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-27 22:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 48.9 MB (32.45%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -2,50 +2,22 @@ class Solution
 {
     public int removeDuplicates(int[] nums) 
     {
-       /* int total=0, f=0;
-        for(int i=0;i<nums.length;i++)
+        if (nums.length <= 2) 
         {
-            f=0;
-            for(int j=0;j<nums.length;j++)
+            return nums.length;
+        }
+
+        int k = 2; 
+
+        for (int i = 2; i < nums.length; i++) 
+        {
+            if (nums[i] != nums[k - 2]) 
             {
-                if(nums[i]==nums[j])
-                {
-                    if(f<3)
-                    {
-                        f++;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-                else
-                {
-                    total=total+f;
-                    i=j;
-                    break;
-                }
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return total;*/
 
-    
-    if (nums.length <= 2) 
-    {
-        return nums.length;
-    }
-
-    int k = 2; 
-
-    for (int i = 2; i < nums.length; i++) 
-    {
-        if (nums[i] != nums[k - 2]) 
-        {
-            nums[k] = nums[i];
-            k++;
-        }
-    }
-
-    return k;
+        return k;
     }
 }
