d9fc5a3a854fceced3cfe9c7b1589420fa5334a3
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-29 15:46:21 +0530

    Time: 303 ms (42.44%) | Memory: 46.5 MB (90.12%) - LeetSync

--- 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.java
+++ 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.java
@@ -1,17 +1,24 @@
-class Solution {
-    public int countTriplets(int[] nums) {
+class Solution 
+{
+    public int countTriplets(int[] nums) 
+    {
         int[] count = new int[1 << 16];
         
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
+        for (int i = 0; i < nums.length; i++) 
+        {
+            for (int j = 0; j < nums.length; j++) 
+            {
                 count[nums[i] & nums[j]]++;
             }
         }
         
         int ans = 0;
-        for (int x : nums) {
-            for (int v = 0; v < (1 << 16); v++) {
-                if ((x & v) == 0) {
+        for (int x : nums) 
+        {
+            for (int v = 0; v < (1 << 16); v++) 
+            {
+                if ((x & v) == 0) 
+                {
                     ans += count[v];
                 }
             }
