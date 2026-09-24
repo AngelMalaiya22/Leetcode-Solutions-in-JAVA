5ee31e00e47b6544214c8a5c82205b0730009748
Angel Malaiya <264592342+AngelMalaiya22@users.noreply.github.com>
2026-09-24 16:05:27 +0530

    Time: 3 ms (99.88%) | Memory: 74.7 MB (39.77%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,9 +1,46 @@
-import java.util.Arrays;
 class Solution 
 {
     public int findKthLargest(int[] nums, int k) 
     {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        int min = nums[0];  //Calculated the minimum element
+        for (int i = 0; i < nums.length; i++) 
+        {
+            if (min > nums[i]) 
+            {
+                min = nums[i];
+            }
+        }
+
+        int max = nums[0];  //Calculatedthe maximum element
+        for (int i = 0; i < nums.length; i++) 
+        {
+            if (max < nums[i]) 
+            {
+                max = nums[i];
+            }
+        }
+
+        int len = max - min + 1;  // Calculated the array length 
+        int arr[] = new int[len]; // Decleared the new array
+
+        for (int i = 0; i < nums.length; i++)  //Counting frequeny of every element and if element is not present then it is storing zero in it 
+        {
+            int index = nums[i] - min;    //value of arr-min
+            arr[index]++;
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--)  
+        {
+            if (arr[i] > 0) 
+            {
+                k = k - arr[i];
+                if (k <= 0) //Got the kth largest element
+                {
+                    return i + min; 
+                }
+            }
+        }
+
+        return -1;
     }
 }
